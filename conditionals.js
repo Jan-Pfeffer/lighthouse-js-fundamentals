06d27f998a06bbd23ced573dc0d29bf8fd88b1ff
Jan Pfeffer <jan@pfeffers.space>
2022-01-15 09:42:10 -0400

    Add logic operations

--- conditionals.js
+++ conditionals.js
@@ -1,12 +1,21 @@
-const temperature = 12;
+/*
+const temperature = 15;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
+if (temperature < 4) {
+    console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
+    console.log("Short sleeves won't cut it!");
 } else {
-  console.log("Short sleeves are fine.");
+    console.log("Short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside!");
+*/
 
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+    console.log("You are eligible to vote.");
+}
