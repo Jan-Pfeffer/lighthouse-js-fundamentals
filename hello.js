b20fdf4f05cbac264d086c6412129cd7bc746c3c
Jan Pfeffer <jan@pfeffers.space>
2022-01-15 12:30:14 -0400

    hello.js: Added more functions

--- hello.js
+++ hello.js
@@ -3,8 +3,39 @@ const sayHello = function() {
     console.log("Hello, world");
 }
 */
+
+/*
 sayHello();
 
 const sayHello = function(name) {
     console.log("Hello, " + name);
 }
+*/
+
+/*
+const sayHelloToConsole = function(name) {
+    console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+*/
+/*
+const returnSayHello = function(name) {
+    return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+*/
+
+/*
+const returnSayHello = function(name) {
+    return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+*/
+
+/*
+const returnSayHello = function(name) {
+    return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+*/
