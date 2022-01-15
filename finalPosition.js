be017f07428febfe42f49c222600b3495e86af95
Jan Pfeffer <jan@pfeffers.space>
2022-01-15 19:07:16 -0400

    finalPosition.js: Added Logic

--- finalPosition.js
+++ finalPosition.js
@@ -1,17 +1,22 @@
 const attitude = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
 let vertical = 0
 let horizontal = 0
-for (let direction of attitude) {
-    if (direction === "north") {
-        vertical = vertical + 1
-    } else if (direction === "south") {
-        vertical = vertical - 1
-    } else if (direction === "east") {
-        horizontal = horizontal + 1
-    } else if (direction === "west") {
-        horizontal = horizontal - 1
+let loc = []
+
+function finalPosition(attitude) {
+    for (let direction of attitude) {
+        if (direction === "north") {
+            vertical = vertical + 1
+        } else if (direction === "south") {
+            vertical = vertical - 1
+        } else if (direction === "east") {
+            horizontal = horizontal + 1
+        } else if (direction === "west") {
+            horizontal = horizontal - 1
+        }
     }
+    loc.push(horizontal, vertical)
+    return loc;
 }
 
-console.log(vertical);
-console.log(horizontal);
+finalPosition(attitude);
