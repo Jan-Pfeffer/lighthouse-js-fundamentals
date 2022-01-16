35193c48a475da44e0547a75e2819af87d53f694
Jan Pfeffer <jan@pfeffers.space>
2022-01-15 20:07:13 -0400

    ageCalculator: Added Logic

---
+++ ageCalculator.js
@@ -0,0 +1,10 @@
+let bYear = 1950
+let cYear = 2000
+let name = "Jeff"
+
+function ageCalculator(name, bYear, cYear) {
+    return (name + " is " + (cYear - bYear) + " years old.");
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
