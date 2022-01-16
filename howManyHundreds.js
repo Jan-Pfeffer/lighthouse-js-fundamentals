6f566a9c1d38449a5a7425137f113b9e5ef03b80
Jan Pfeffer <jan@pfeffers.space>
2022-01-15 20:06:47 -0400

    howManyHundreds: Added logic

---
+++ howManyHundreds.js
@@ -0,0 +1,10 @@
+function howManyHundreds(x) {
+    let num = Math.floor(x / 100);
+    return num;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
