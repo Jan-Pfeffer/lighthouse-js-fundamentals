51cbd231dd3e3298deda9fc1a4e2b97c4a40c2f9
Jan Pfeffer <jan@pfeffers.space>
2022-01-15 20:08:55 -0400

    chooseStation: Switched For loop to while loop

--- chooseStation.js
+++ chooseStation.js
@@ -1,25 +1,26 @@
-let deemedAppropriate = []
 let x = 0;
-/*chooseStations(stations)*/
-
+let deemedAppropriate = []
 
 const stations = [
     ['Big Bear Donair', 10, 'restaurant'],
-    ['Bright Lights Elementary', 50, 'school'],
-    ['Moose Mountain Community Centre', 45, 'community centre']
+    ["Bright Lights Elementary", 50, 'school'],
+    ['Moose Mountain Community Centre', 10, 'community centre']
 ];
 
-
-for (station in stations) {
-    let innerArray = stations[x]
-    let name = innerArray[0]
-    let capacity = innerArray[1]
-    let venue = innerArray[2]
-    if (capacity >= 20 && (venue == 'school' || venue == 'community centre')) {
-        deemedAppropriate.push(name)
+function chooseStations(stations) {
+    const arrayLength = stations.length;
+    while (x < arrayLength) {
+        let innerArray = stations[x];
+        let name = innerArray[0];
+        let capacity = innerArray[1];
+        let venue = innerArray[2];
+        if (capacity >= 20 && (venue == 'school' || venue == 'community centre')) {
+            deemedAppropriate.push(name);
+        }
+        x++;
     }
-    x++;
+    return deemedAppropriate;
 
 }
 
-console.log(deemedAppropriate);
+console.log(chooseStations(stations));
