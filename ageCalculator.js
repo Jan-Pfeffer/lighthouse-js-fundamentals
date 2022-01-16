7142f0b9cd7fe8db695524c0a4fa39b87cb84a12
Jan Pfeffer <jan@pfeffers.space>
2022-01-16 14:54:58 -0400

    ageCalculator: Chnaged to arrow function

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,10 +1,5 @@
-let bYear = 1950
-let cYear = 2000
-let name = "Jeff"
+let ageCalculator = (name, bYear, cYear) => (name + " is " + (cYear - bYear) + " years old.");
 
-function ageCalculator(name, bYear, cYear) {
-    return (name + " is " + (cYear - bYear) + " years old.");
-}
 
 console.log(ageCalculator("Miranda", 1983, 2015));
 console.log(ageCalculator("Ferdinand", 1988, 2015));
